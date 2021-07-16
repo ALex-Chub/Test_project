130b96c48b94a530b047969f9c1928b9194be566
AlexFizMath <83985868+ALex-Chub@users.noreply.github.com>
2021-07-16 21:54:17 +0300

    test4: stong chaos game - catch circle .

--- src/com/company/Game_window.java
+++ src/com/company/Game_window.java
@@ -2,23 +2,28 @@ package com.company;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class Game_window  extends JFrame {
 
-    private  static int window_weigth = 800;
-    private  static int window_height = 700;
-    private  static int drop_diam = 100;
+    private  static final int window_weigth = 1000;
+    private  static final int window_height = 700;
+    private  static final int drop_diam = 100;
     private  static Game_window game_window;
     private  static long last_frame_time;
     private  static Image background;
     private  static Image drop;
     private  static Image  game_over;
-    private  static float drop_left = 200;
-    private  static float drop_top = 200;
-    private static  float speed = 200;
-    private static  int top_speed_mod = 1;
-    private static int left_speed_mod = 1;
+    private  static double drop_left = 200;
+    private  static double drop_top = 200;
+    private static  double top_speed = 200;
+    private static  double left_speed = 200;
+    private static  double max_speed = 400;
+    private static  double top_speed_mod = 1;
+    private static double left_speed_mod = 1;
+    private  static  int score = 0;
 
 
     public static void main(String[] args) throws IOException {
@@ -32,6 +37,28 @@ public class Game_window  extends JFrame {
         game_window.setResizable(false);
         last_frame_time = System.nanoTime();
         GameField gameField = new GameField();
+        gameField.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                double drop_right = drop_left+drop_diam;
+                double drop_bottom = drop_top+drop_diam;
+                boolean is_drop = (x>=drop_left && x <= drop_right && y >= drop_top && y<= drop_bottom);
+                if (is_drop){
+                    drop_top=(int)(Math.random()*(window_height/10*8));
+                    drop_left=(int)(Math.random()*(window_weigth/10*8));
+                    left_speed=(Math.random()*max_speed+250);
+                    top_speed=Math.random()*max_speed+250;
+                    score=score+5;
+                }else {
+                    score=score-2;
+                }
+                game_window.setTitle("Score: "+score);
+
+            }
+        });
+
         game_window.add(gameField);
 
         game_window.setVisible(true);
@@ -44,27 +71,26 @@ public class Game_window  extends JFrame {
         last_frame_time = parent_time;
 
 
-        drop_top=drop_top+speed*delta_time*top_speed_mod;
-        drop_left=drop_left+speed*delta_time*left_speed_mod;
+        drop_top=drop_top+top_speed*delta_time*top_speed_mod;
+        drop_left=drop_left+left_speed*delta_time*left_speed_mod;
 
 
-        if (drop_top>window_height-drop_diam){
-           top_speed_mod=0-top_speed_mod;
+        if (drop_top>window_height-drop_diam||drop_top<0){
+           top_speed_mod=-top_speed_mod;
+           top_speed=Math.random()*max_speed+250;
+            left_speed=Math.random()*max_speed+250;
+            score--;
         }
-        if (drop_top<0){
-            top_speed_mod=0-top_speed_mod;
+        if(drop_left<0||drop_left>window_weigth-drop_diam){
+            left_speed_mod=-left_speed_mod;
+            left_speed=Math.random()*max_speed+250;
+            top_speed=Math.random()*max_speed+250;
+            score--;
         }
-        if(drop_left<0){
-            left_speed_mod=0-left_speed_mod;
-        }
-        if(drop_left>window_weigth-drop_diam){
-            left_speed_mod=0-left_speed_mod;
-        }
-
 
         graphics.drawImage(background, 0 , 0 , null);
         graphics.drawImage(drop, (int)drop_left, (int)drop_top, drop_diam,drop_diam,null);
-
+        game_window.setTitle("Score: "+score);
     }
 
     private static class GameField extends JPanel{
